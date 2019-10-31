f3ec16df76bae15901622fb7374c061edf0fe88d
swd <ahmed.sayed.fcis1997@gmail.com>
2019-10-31 16:24:25 +0200

    before leave

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -13,79 +13,79 @@ export class ContactUsComponent implements OnInit {
   description = ''
   phoneNumber = ''
   category = ''
-  // errorObject
+  errorObject
 
   constructor(private http: HttpClient) {
   
    }
 
   ngOnInit() {
-    // this.errorObject = {
-    //   name: true,
-    //   email: true,
-    //   description: true,
-    //   phoneNumber: true,
-    //   category: true
-    // }
+    this.errorObject = {
+      name: true,
+      email: true,
+      description: true,
+      phoneNumber: true,
+      category: true
+    }
   }
   
 
   nameChangeInput() {
-    // console.log(this.fullName)
-    // if(this.fullName === '' || this.fullName === null || this.fullName.length < 10 ) {
-    //   this.errorObject.name = true
-    // } else {
-    //   this.errorObject.name = false
-    // }
+    console.log(this.fullName)
+    if(this.fullName === '' || this.fullName === null || this.fullName.length < 10 ) {
+      this.errorObject.name = true
+    } else {
+      this.errorObject.name = false
+    }
 
-    // console.log(this.errorObject)
-    // this.disabledSubmit()
+    console.log(this.errorObject)
+    this.disabledSubmit()
   }
 
   emailChangeInput() {
-    // if(this.email === '' || this.email === null || this.email.length < 10) {
-    //   this.errorObject.email = true
-    // } else {
-    //   this.errorObject.email = false
-    // }
+    if(this.email === '' || this.email === null || this.email.length < 10) {
+      this.errorObject.email = true
+    } else {
+      this.errorObject.email = false
+    }
 
-    // console.log(this.errorObject)
-    // this.disabledSubmit()
+    console.log(this.errorObject)
+    this.disabledSubmit()
   }
 
   phoneNumberChangeinput() {
-    // if(this.phoneNumber === '' || this.phoneNumber === null || this.phoneNumber.length < 11 || this.phoneNumber.length > 11 ) {
-    //   this.errorObject.phoneNumber = true
-    // } else {
-    //   this.errorObject.phoneNumber = false
-    // }
+    if(this.phoneNumber === '' || this.phoneNumber === null || this.phoneNumber.length < 11 || this.phoneNumber.length > 11 ) {
+      this.errorObject.phoneNumber = true
+    } else {
+      this.errorObject.phoneNumber = false
+    }
 
-    // console.log(this.errorObject)
-    // this.disabledSubmit()
+    console.log(this.errorObject)
+    this.disabledSubmit()
   }
 
   descriptionChangeInput() {
-    // if(this.description === '' || this.description === null || this.description.length < 50) {
-    //   this.errorObject.description = true
-    // } else {
-    //   this.errorObject.description = false
-    // }
+    if(this.description === '' || this.description === null || this.description.length < 50) {
+      this.errorObject.description = true
+    } else {
+      this.errorObject.description = false
+    }
 
-    // console.log(this.errorObject)
-    // this.disabledSubmit()
+    console.log(this.errorObject)
+    this.disabledSubmit()
   }
 
   categoryChangeInput() {
-    // if(this.category === '' || this.category === null) {
-    //   if(this.description !== 'Automobile' && this.description !== 'MotorCycle' && this.description !== 'Marine' && this.description !== 'Power-Product') {
-    //     this.errorObject.category = true
-    //   }else {
-    //     this.errorObject.category = false
-    //   }
-    // } 
+    if(this.category === '' || this.category === null) {
+      if(this.description !== 'Automobile' && this.description !== 'MotorCycle' && this.description !== 'Marine' && this.description !== 'Power-Product') {
+        this.errorObject.category = true
+      }else {
+        this.errorObject.category = false
+      }
+    } 
 
-    // console.log(this.errorObject)
-    // this.disabledSubmit()
+    console.log(this.errorObject)
+    this.disabledSubmit()
   }
 
   disabledSubmit() {
@@ -95,8 +95,7 @@ export class ContactUsComponent implements OnInit {
     
     let headers = new HttpHeaders({
       'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT',
-      'Access-Control-Allow': '*',
-      'test': 'test' });
+      'Access-Control-Allow': '*', });
     let options = { headers: headers };
 
     console.log('send data fron severe')
