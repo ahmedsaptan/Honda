b3bc7bd717c0e07eca87b4caab43fd0be6c71a7c
swd <ahmed.sayed.fcis1997@gmail.com>
2019-10-31 11:40:52 +0200

    finish and test api

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact-us',
@@ -12,87 +13,101 @@ export class ContactUsComponent implements OnInit {
   description = ''
   phoneNumber = ''
   category = ''
-  errorObject
+  // errorObject
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  
+   }
 
   ngOnInit() {
-    this.errorObject = {
-      name: true,
-      email: true,
-      description: true,
-      phoneNumber: true,
-      category: true
-    }
+    // this.errorObject = {
+    //   name: true,
+    //   email: true,
+    //   description: true,
+    //   phoneNumber: true,
+    //   category: true
+    // }
   }
   
 
   nameChangeInput() {
-    console.log(this.fullName)
-    if(this.fullName === '' || this.fullName === null || this.fullName.length < 10 ) {
-      this.errorObject.name = true
-    } else {
-      this.errorObject.name = false
-    }
+    // console.log(this.fullName)
+    // if(this.fullName === '' || this.fullName === null || this.fullName.length < 10 ) {
+    //   this.errorObject.name = true
+    // } else {
+    //   this.errorObject.name = false
+    // }
 
-    console.log(this.errorObject)
-    this.disabledSubmit()
+    // console.log(this.errorObject)
+    // this.disabledSubmit()
   }
 
   emailChangeInput() {
-    if(this.email === '' || this.email === null || this.email.length < 10) {
-      this.errorObject.email = true
-    } else {
-      this.errorObject.email = false
-    }
+    // if(this.email === '' || this.email === null || this.email.length < 10) {
+    //   this.errorObject.email = true
+    // } else {
+    //   this.errorObject.email = false
+    // }
 
-    console.log(this.errorObject)
-    this.disabledSubmit()
+    // console.log(this.errorObject)
+    // this.disabledSubmit()
   }
 
   phoneNumberChangeinput() {
-    if(this.phoneNumber === '' || this.phoneNumber === null || this.phoneNumber.length < 11 || this.phoneNumber.length > 11 ) {
-      this.errorObject.phoneNumber = true
-    } else {
-      this.errorObject.phoneNumber = false
-    }
+    // if(this.phoneNumber === '' || this.phoneNumber === null || this.phoneNumber.length < 11 || this.phoneNumber.length > 11 ) {
+    //   this.errorObject.phoneNumber = true
+    // } else {
+    //   this.errorObject.phoneNumber = false
+    // }
 
-    console.log(this.errorObject)
-    this.disabledSubmit()
+    // console.log(this.errorObject)
+    // this.disabledSubmit()
   }
 
   descriptionChangeInput() {
-    if(this.description === '' || this.description === null || this.description.length < 50) {
-      this.errorObject.description = true
-    } else {
-      this.errorObject.description = false
-    }
+    // if(this.description === '' || this.description === null || this.description.length < 50) {
+    //   this.errorObject.description = true
+    // } else {
+    //   this.errorObject.description = false
+    // }
 
-    console.log(this.errorObject)
-    this.disabledSubmit()
+    // console.log(this.errorObject)
+    // this.disabledSubmit()
   }
 
   categoryChangeInput() {
-    if(this.category === '' || this.category === null) {
-      if(this.description !== 'Automobile' && this.description !== 'MotorCycle' && this.description !== 'Marine' && this.description !== 'Power-Product') {
-        this.errorObject.category = true
-      }else {
-        this.errorObject.category = false
-      }
-    } 
+    // if(this.category === '' || this.category === null) {
+    //   if(this.description !== 'Automobile' && this.description !== 'MotorCycle' && this.description !== 'Marine' && this.description !== 'Power-Product') {
+    //     this.errorObject.category = true
+    //   }else {
+    //     this.errorObject.category = false
+    //   }
+    // } 
 
-    console.log(this.errorObject)
-    this.disabledSubmit()
+    // console.log(this.errorObject)
+    // this.disabledSubmit()
   }
 
   disabledSubmit() {
-    return !(this.errorObject.description && this.errorObject.name && this.errorObject.email && this.errorObject.phoneNumber && this.errorObject.category)
+    // return !(this.errorObject.description && this.errorObject.name && this.errorObject.email && this.errorObject.phoneNumber && this.errorObject.category)
   }
   sendDataToserver() {
-    console.log(this.category)
-    console.log(this.email)
-    console.log(this.description)
-    console.log(this.phoneNumber)
-    console.log(this.fullName)
+    
+    let headers = new HttpHeaders({
+      'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT',
+      'Access-Control-Allow': '*',
+      'test': 'test' });
+    let options = { headers: headers };
+
+    console.log('send data fron severe')
+    this.http.post('https://mivors--tst3.custhelp.com/cc/Test/Create_Contact', {
+      Name: this.fullName,
+      Email: this.email,
+      Number: this.phoneNumber,
+      Category: this.category,
+      Description: this.description
+    }, options).subscribe(data => {
+      console.log(data)
+    })
   }
 }
